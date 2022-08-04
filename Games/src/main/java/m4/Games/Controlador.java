10db888e4e6f1dfe497b357f98e44378fbac998f
ionela vladasel <ionelavladasel2710@gmail.com>
2022-08-04 12:54:40 +0200

    deleted comms

--- Games/src/main/java/m4/Games/Controlador.java
+++ Games/src/main/java/m4/Games/Controlador.java
@@ -62,7 +62,7 @@ public class Controlador {
 		}
 	}
 
-	public void actionResolver() {// david
+	public void actionResolver() {
 		ventana.getBtnResolver().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				// Confirmar siguiente partida
@@ -86,7 +86,7 @@ public class Controlador {
 		});
 	}
 
-	public void actionPista() {// ionela
+	public void actionPista() {
 		ActionListener pistaListener = new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
@@ -113,7 +113,7 @@ public class Controlador {
 
 	}
 
-	public void actionBotonesLetras() {// joana
+	public void actionBotonesLetras() {
 		ActionListener letrasListener = new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
@@ -138,18 +138,12 @@ public class Controlador {
 					ventana.cambiarImagen(Partida.INTENTOS_MAX - juego.getPartida().getIntentos());
 					
 				}
-				/*
-				 * acceder a los asteriscos por posicion y ver cuales hay que cambiar, segun los
-				 * datos guardados en lanterior arraylist. Podriamos hacer que
-				 * ventana.ponerPalabraSecreta(xxx) devolviera la palabra hecha asteriscos, pero
-				 * como ese metodo mete la palabra en JLabel, como se modificarian las letras
-				 * por separado de un JLabel??
-				 */
-
+				
 				// en cualquier caso se desactiva botonLetra
 				tecla.setEnabled(false);
 
-				// juego.comprobarFinPartida();
+				// partida.comprobarFinPartida();
+				//juego.comprobarFinJuego();
 			}
 
 		};
@@ -162,14 +156,5 @@ public class Controlador {
 		}
 
 	}
-	/*
-	 * public Juego getJuego() { return juego; }
-	 * 
-	 * public Ventana getVentana() { return ventana; }
-	 */
-
-	/*
-	 * hacer barra de menu con boton de ayuda y boton about us
-	 */
 
 }
